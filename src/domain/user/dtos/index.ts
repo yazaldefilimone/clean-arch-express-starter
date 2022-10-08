e593c443fdaa54289686659c032f7e649cea8e28
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-08 21:13:48 +0200

    feat: create user dtos

---
+++ src/domain/user/dtos/index.ts
@@ -0,0 +1,18 @@
+export type userType = {
+  name: string;
+  email: string;
+  password: string;
+};
+
+export type userLoginType = {
+  email: string;
+  password: string;
+};
+
+export type userStoreType = {
+  id: string;
+  email: string;
+  password: string;
+  createdAt: string;
+  updateAt: string;
+};
