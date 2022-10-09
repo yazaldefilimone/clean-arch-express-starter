f1f19886196a8d1eb2d4330aac28f39f399ae70d
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-09 16:43:06 +0200

    chore: config jest

---
+++ jest.config.ts
@@ -0,0 +1,20 @@
+// eslint-disable-next-line import/extensions
+/* eslint-disable import/extensions */
+const eslintConfig = {
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!<rootDir>/src/**/index.ts'],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  moduleNameMapper: {
+    '~/tests/(.+)': '<rootDir>/tests/$1',
+    '~/(.+)': '<rootDir>/src/$1',
+  },
+  testMatch: ['**/*.spec.ts'],
+  roots: ['<rootDir>/src', '<rootDir>/tests'],
+  transform: {
+    '\\.ts$': 'ts-jest',
+  },
+  clearMocks: true,
+  setupFiles: ['dotenv/config'],
+};
+
+export default eslintConfig;
