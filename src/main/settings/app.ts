f9a238714958de79b881121142f7e6f9bf59b1b3
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-16 20:44:47 +0200

    feat: register router sign in app

--- src/main/settings/app.ts
+++ src/main/settings/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import { userRoutes } from '~/main/routes/user';
 
 const app = express();
 
@@ -8,5 +9,5 @@ app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use('/user', userRoutes);
 export { app };
