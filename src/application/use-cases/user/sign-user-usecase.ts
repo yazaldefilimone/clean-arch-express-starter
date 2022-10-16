6d8da4d9260d14cbece48b6804af8ee609fdadfa
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-16 15:24:32 +0200

    feat: create sign user

--- src/application/use-cases/user/sign-user-usecase.ts
+++ src/application/use-cases/user/sign-user-usecase.ts
@@ -1,5 +1,8 @@
 import { IUserRepository } from '~/application/repositories/user';
+import { AlreadyExistsError } from '~/domain/errors';
+import { User } from '~/domain/user/entity';
 import { ISignUserUseCase } from '~/domain/user/use-cases';
+import { left, right } from '~/shared/either';
 
 export class SignUserUseCase implements ISignUserUseCase {
   private readonly userRepository: IUserRepository;
@@ -8,6 +11,17 @@ export class SignUserUseCase implements ISignUserUseCase {
   }
 
   async perform(input: ISignUserUseCase.Input): ISignUserUseCase.Output {
-    return null;
+    const building = User.build(input);
+
+    if (building.isLeft()) return left(building.value);
+    const user = building.value;
+
+    const userExists = this.userRepository.getEmail({ email: user.email });
+    if (userExists) {
+      return left(new AlreadyExistsError({ param: 'user' }));
+    }
+    const userStore = await this.userRepository.sign(user);
+
+    return right(userStore);
   }
 }
