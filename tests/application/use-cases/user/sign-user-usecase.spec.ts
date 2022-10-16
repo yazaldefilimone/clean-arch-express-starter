7a9e260926bec2c23bdf45659debf62b3765f904
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-16 18:30:07 +0200

    test: ensure  return AlreadyExistsError if add two emails

---
+++ tests/application/use-cases/user/sign-user-usecase.spec.ts
@@ -0,0 +1,33 @@
+import { SignUserUseCase } from '~/application/use-cases/user';
+import { AlreadyExistsError } from '~/domain/errors';
+import { userType } from '~/domain/user/dtos';
+import { InMemoryUserRepository } from '~/tests/infrastructure/repositories/user';
+
+const makeSut = () => {
+  const fakePayload: userType = {
+    name: 'John Doe',
+    email: 'johndoe@gmail.com',
+    password: 'johndoe@2020',
+  };
+
+  const userRepository = new InMemoryUserRepository();
+  const sut = new SignUserUseCase(userRepository);
+  return {
+    sut,
+    userRepository,
+    fakePayload,
+  };
+};
+
+describe('SignUserUseCase', () => {
+  it('Should return AlreadyExistsError if add two emails', async () => {
+    const { sut, userRepository, fakePayload } = makeSut();
+    await sut.perform(fakePayload);
+    const response = await sut.perform(fakePayload);
+    expect(userRepository.getEmailCalls).toBe(2);
+    expect(userRepository.signCalls).toBe(1);
+    expect(response.isLeft()).toBe(true);
+    expect(response.isRight()).toBe(false);
+    expect(response.value).toEqual(new AlreadyExistsError({ param: 'user' }));
+  });
+});
