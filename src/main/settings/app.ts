7ced2435d11bf581588128f4b0ad5e95112e6009
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-16 20:11:50 +0200

    feat: create app express

---
+++ src/main/settings/app.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+export { app };
