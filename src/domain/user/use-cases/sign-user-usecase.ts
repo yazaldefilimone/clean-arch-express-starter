a4c59f0199fe841e80686fe07a702443d2024ec6
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-16 15:23:27 +0200

    feat: using AlreadyExistsError

--- src/domain/user/use-cases/sign-user-usecase.ts
+++ src/domain/user/use-cases/sign-user-usecase.ts
@@ -1,13 +1,14 @@
 import { Either } from '~/shared/either';
 import { userType, userStoreType } from '~/domain/user/dtos';
 import { InvalidParamError, InternalServerError } from '~/domain/errors';
+import { AlreadyExistsError } from '~/domain/errors/already-exists-error';
 
 export interface ISignUserUseCase {
   perform: (input: ISignUserUseCase.Input) => ISignUserUseCase.Output;
 }
 
-type signUserUseCaseFailed = InvalidParamError | InternalServerError;
+type signUserUseCaseFailed = InvalidParamError | InternalServerError | AlreadyExistsError;
 export namespace ISignUserUseCase {
   export type Input = userType;
-  export type Output = Promise<Either<signUserUseCaseFailed, userStoreType>>;
+  export type Output = Promise<Either<signUserUseCaseFailed, { id: string }>>;
 }
