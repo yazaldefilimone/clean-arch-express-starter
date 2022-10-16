694dce9bec17574ce813ae9839916c8f355f1f72
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-10-16 15:23:57 +0200

    refactor: lint format

--- src/domain/errors/not-found-error.ts
+++ src/domain/errors/not-found-error.ts
@@ -1,6 +1,6 @@
 export class NotFoundError extends Error {
-  constructor ({ param }: { param: string }) {
-    super(`this [${param}] was not found.`)
-    this.name = 'NotFoundError'
-  }
+	constructor({ param }: { param: string }) {
+		super(`this [${param}] was not found.`);
+		this.name = 'NotFoundError';
+	}
 }
